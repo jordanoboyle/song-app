21235fa6e2830ad4bbd38ce2ce6d26ab48eafb43
jordanoboyle <jmcmcode@gmail.com>
2024-05-16 13:13:26 -0600

    show test added

--- test/controllers/songs_controller_test.rb
+++ test/controllers/songs_controller_test.rb
@@ -19,4 +19,13 @@ class SongsControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal Song.count, data.length
   end
+
+  test "show" do
+    get "/songs/#{Song.first.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal ["id", "title", "artist", "length", "created_at", "updated_at"], data.keys
+  end
+
 end
